d5f2d0aaf50e7fd2d0e451772e77800926a84917
guhdalla <guhdalla011@gmail.com>
2021-11-23 21:33:05 -0300

    Initial

---
+++ src/main/java/com/bloodagency/BloodagencyApplication.java
@@ -0,0 +1,13 @@
+package com.bloodagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodagencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodagencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloodagency/BloodagencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodagencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
